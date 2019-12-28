cf70d4ee884bfe964f9d208a11fc7a5354f6ca00
Habib ul Rehman <mhabibulrehman@gmail.com>
2019-12-28 18:00:32 +0500

    Add project files.

---
+++ Chronic Disease Prediction/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Chronic_Disease_Prediction
+{
+    public class DataInput
+    {
+        [LoadColumn(0)]
+        public int Age { get; set; }
+    }
+}
---
+++ Chronic Kidney Disease Prediction/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Chronic_Kidney_Disease_Prediction.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
